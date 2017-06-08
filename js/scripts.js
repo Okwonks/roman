6a5f4f8dd7d1e1c45c921733b84305cc5e8b19fd
Okwonks <albertoketch@gmail.com>
2017-06-08 15:52:31 +0300

    adding styles and some jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,30 @@
 //business logic
+var convertNumber = function(num) {
 
+  var decimalNumber = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  var result = "";
+  if (num <= 3999) {
+  for (index = 0; index <= decimalNumber.length; index++) {
+    while (decimalNumber[index] <= num) {
+      result += romanNumeral[index];
+      num -= decimalNumber[index];
+    }
+  }
+return result;
+  } else {
+    return "not a roman number";
+  }
+};
   //user interface logic
 $(document).ready(function(){
     $("form#roman-1").submit(function(event) {
         event.preventDefault();
           var input = parseInt($("input#roman").val());
-          var result = "";
-          var convertNumber = function(input) {
+          var romanised = convertNumber(input);
 
-            var decimalNumber = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-            var romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+          $(".roman").text(romanised);
 
-
-
-            for (index = 0; index <= decimalNumber.length; index++) {
-              while (decimalNumber[index] <= input) {
-                result += romanNumeral[index];
-                input -= decimalNumber[index];
-              }
-            }
-          return result;
-            }
-          convertNumber(input);
-
-          $(".roman").text(result);
-
-         $("#answer").show();
+         $("#answer").fadeIn(2000).fadeOut(4000);
         });
       });
